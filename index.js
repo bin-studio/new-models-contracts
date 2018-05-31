4dd25ed5811f5449294955e7599a6750a510f85c
Billy Rennekamp <billy.rennekamp@gmail.com>
2018-05-31 19:26:03 +0200

    main

---
+++ index.js
@@ -0,0 +1,11 @@
+var Patches = require("./build/contracts/Patches.json");
+var Controller = require("./build/contracts/Controller.json");
+var Metadata = require("./build/contracts/Metadata.json");
+var IPatches = require("./build/contracts/IClovers.json");
+
+module.exports = {
+  Patches: Patches,
+  Controller: Controller,
+  Metadata: Metadata,
+  IPatches: IPatches
+};
